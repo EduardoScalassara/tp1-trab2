9bde9c2de533935184d25c319559a44badd38cc8
EduardoScalassara <140093525+EduardoScalassara@users.noreply.github.com>
2025-01-25 15:49:20 -0300

    Update dominios.hpp
    GENTE USEM A CLASSE TEMPLATE PFV VCS N VAO SE ARREPENDER

--- dominios.hpp
+++ dominios.hpp
@@ -1,1 +1,20 @@
+#ifndef DOMINIOS_HPP_INCLUDED
+#define DOMINIOS_HPP_INCLUDED
 
+template <typename T> 
+class Dominio {
+  private:
+    T valor;
+    virtual bool validar(T) = 0;  
+  public:
+    bool setValor(T);
+    T getValor() const;
+};
+
+inline T Dominio<T>::getValor() const {
+  return valor;
+}
+
+#endif // DOMINIOS_HPP_INCLUDED
+    
+    

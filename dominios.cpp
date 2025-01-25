b74e331ad945361e06b766d7bf1102abddc891bf
EduardoScalassara <140093525+EduardoScalassara@users.noreply.github.com>
2025-01-25 16:22:38 -0300

    Update dominios.cpp

--- dominios.cpp
+++ dominios.cpp
@@ -1,4 +1,8 @@
+#include <iostream>
 #include "dominios.hpp"
+
+template <typename T>
+
 
 bool Dominio<T>::setValor(T) {
   if (!validar(valor))
@@ -7,4 +11,31 @@ bool Dominio<T>::setValor(T) {
   return true;
 }
 
+template <typename T>
+bool DominioA1<T>::validar(T valor) {
+    std::cout << "Local para codigo validacao DominioA1" << "\n";
+
+    return true;
+}
+
+template <typename T>
+bool DominioA2<T>::validar(T valor) {
+    std::cout << "Local para codigo validacao DominioA2" << "\n";
+
+    return true;
+}
+
+template <typename T>
+bool DominioB1<T>::validar(T valor) {
+    std::cout << "Local para codigo validacao DominioB1" << "\n";
+
+    return true;
+}
+
+template <typename T>
+bool DominioB2<T>::validar(T valor) {
+    std::cout << "Local para codigo validacao DominioB2" << "\n";
+
+    return true;
+}
 
